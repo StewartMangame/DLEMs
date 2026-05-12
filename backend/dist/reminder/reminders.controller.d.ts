2795e5928f5be87c5f78fa451782f67c1a93aeac
WilliamSato77 <ericsato77@gmail.com>
2026-05-12 20:24:47 +0200

    Fix rendering error

--- backend/dist/reminder/reminders.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from 'typeorm';
-import { Reminder } from '../entities/reminder.entity';
-export declare class RemindersController {
-    private reminderRepo;
-    constructor(reminderRepo: Repository<Reminder>);
-    getReminders(req: any): Promise<{
-        reminders: Reminder[];
-    }>;
-}
